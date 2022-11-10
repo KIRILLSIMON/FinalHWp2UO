d4d8a50d2570e329038ee6375922a81e8cc5da6c
sikir <sikir2022@gmail.com>
2022-11-10 15:34:37 +0300

    V nazvanii dopustil 1u oploshnost'(netochnost')-eto predfinalnaya chast DZ 1go uroka Simonova KO.

---
+++ src/Main.java
@@ -0,0 +1,63 @@
+public class Main {
+    public static void main(String[] args) {
+        /*System.out.println("Reshenie z4 UO p.2");
+        int distfromBtoCHinKm=18;
+        int timeToClientsHomeInDays=1;
+        if (distfromBtoCHinKm<=20){
+            timeToClientsHomeInDays+=0;
+            System.out.println(timeToClientsHomeInDays);
+        } else if(distfromBtoCHinKm<=60) {
+            timeToClientsHomeInDays+=1;
+            System.out.println(timeToClientsHomeInDays);
+        } else {
+            System.out.println("Карта приедет за 3дня");}*/
+System.out.println("Zadacha pro mesyaci i sezoni v godu");
+int mounthOfYearNumber=7;
+switch (mounthOfYearNumber) {
+    case 1:
+        System.out.println("January/winter");
+        break;
+    case 2:
+        System.out.println("February/");
+        break;
+    case 3:
+        System.out.println("March/");
+        break;
+    case 4:
+        System.out.println("April/spring");
+        break;
+    case 5:
+        System.out.println("May/spring");
+        break;
+    case 6:
+        System.out.println("June/summer");
+        break;
+    case 7:
+        System.out.println("July/summer");
+        break;
+    case 8:
+        System.out.println("August/summer");
+        break;
+    case 9:
+        System.out.println("September/autumn");
+        break;
+    case 10:
+        System.out.println("October/autumn");
+        break;
+    case 11:
+        System.out.println("November/autumn");
+        break;
+    case 12:
+        System.out.println("December/winter");
+        break;
+
+    default:
+        System.out.println("Такого месяца НЕ существует!!!");
+
+
+
+        }
+
+    }
+
+}
